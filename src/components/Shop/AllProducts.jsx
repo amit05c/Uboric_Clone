de5bac83b7adfb802c07b0c343d27e49ef934d48
amig05c <amitghosh0595c@gmail.com>
2022-08-25 11:56:19 +0530

    day3

--- src/components/Shop/AllProducts.jsx
+++ src/components/Shop/AllProducts.jsx
@@ -1,10 +1,12 @@
 import { Box, Flex, Image, Text } from '@chakra-ui/react'
 import React from 'react'
+import {useNavigate} from "react-router-dom"
 
 const AllProducts = ({data}) => {
+  const navigate= useNavigate()
   return (
     <Flex border={"1px solid red"}  direction={"column"} justifyContent="center" alignItems={"center"}
-    
+onClick={()=>navigate(`/shop/${data.id}`)}
     >
         <Image src={data.images} boxSize="200px"  />
         
