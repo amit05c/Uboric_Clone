4338d66a2e02e02411f19014ea7fcb18427e5e25
Hrutugandha Nishigandh ✨✨✨ <hrutunishi19@gmail.com>
2022-08-27 21:33:18 +0530

    Update action.js

--- src/Redux/ContactReducer/action.js
+++ src/Redux/ContactReducer/action.js
@@ -13,13 +13,14 @@ export const getContactMessage = () => (dispatch) => {
       });
   };
 
-  export const updateContactMessage = (id, payload) => (dispatch) => {
+  export const updateContactMessage = (payload) => (dispatch) => {
     dispatch({ type: types.ADD_MESSAGE_REQUEST });
   
     return axios
-      .patch(`http://localhost:8080/Contact/${id}`, payload)
+      .post(`http://localhost:8080/Contact`, payload)
       .then((r) => {
         dispatch({ type: types.ADD_MESSAGE_SUCCESS, payload: r.data });
+        return types.ADD_MESSAGE_SUCCESS;
       })
       .catch((e) => dispatch({ type: types.ADD_MESSAGE_ERROR, payload: e }));
   };
