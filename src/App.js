ea53f18f8414326945200eda772a5f26f3e8955a
Hrutugandha Nishigandh ✨✨✨ <hrutunishi19@gmail.com>
2022-08-27 15:39:37 +0530

    Revert "Merge branch 'main' of https://github.com/amit05c/exclusive-oatmeal-9601"
    This reverts commit 250d2d2d746cdcd4bddd617156392264b9a728f9.

--- src/App.js
+++ src/App.js
@@ -1,20 +1,18 @@
-
-import './App.css';
-import Footer from './components/Footer/Footer';
-import Navbar from './components/Navbar/Navbar';
+import "./App.css";
+import Footer from "./components/Footer/Footer";
+import Navbar from "./components/Navbar/Navbar";
 // import Slider from './components/slider/Slider';
-import AllRoutes from './Pages/AllRoutes';
-import Home from './Pages/Home';
-
+import AllRoutes from "./Pages/AllRoutes";
+import Home from "./Pages/Home";
+import SignUp from "./Pages/SignUp";
 
 function App() {
   return (
     <div className="App">
-      <Navbar/>
+      <Navbar />
       {/* <Home/> */}
-      <AllRoutes/>
-      <Footer/>
-      
+      <AllRoutes />
+      <Footer />
     </div>
   );
 }
